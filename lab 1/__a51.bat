4a922231c648e267524c06c2f0c6248f9ec02473
Ruth Tau <90289622+RuthTau@users.noreply.github.com>
2023-05-02 13:03:16 -0700

    Add files via upload

---
+++ lab 1/__a51.bat
@@ -0,0 +1,5 @@
+@echo off
+C:
+cd "\Users\tauya\Downloads\ELEC 291\"
+"C:\CrossIDE\Call51\Bin\a51.exe" -l "C:\Users\tauya\Downloads\ELEC 291\LCD_test_4bit.asm"
+echo Crosside_Action Set_Hex_File C:\Users\tauya\Downloads\ELEC 291\LCD_test_4bit.HEX
